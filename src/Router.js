994bbdfffbbeb3d344d7e93f9eed8bb5b4af6af9
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2022-01-19 22:07:20 +0100

    separate Router-file

---
+++ src/Router.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import App from './App';
+import { Episodes, PlayEpisode } from "./routes";
+
+const Router = () => {
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App /> } >
+        <Route path=":ageParam" element={<App /> } />
+        <Route path="" element={<App /> } />
+      </Route>
+      <Route path="/episodes/:id/:ageParam" element={<Episodes /> } />
+      <Route path="/audioplay/:id/:url/:imageurl/:title/:ageParam" element={<PlayEpisode /> } />
+      <Route 
+        path="*" 
+        element={
+          <main>
+            <p>There's nothing here!</p>
+          </main>
+        } 
+      />
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default Router; 
--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Router from "./Router"
 import './index.scss';
-import App from './App';
-import { Episodes, PlayEpisode } from "./routes";
-
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App /> } >
-        <Route path=":ageParam" element={<App /> } />
-        <Route path="" element={<App /> } />
-      </Route>
-      <Route path="/episodes/:id/:ageParam" element={<Episodes /> } />
-      <Route path="/audioplay/:id/:url/:imageurl/:title/:ageParam" element={<PlayEpisode /> } />
-      <Route 
-        path="*" 
-        element={
-          <main>
-            <p>There's nothing here!</p>
-          </main>
-        } 
-      />
-    </Routes>
-  </BrowserRouter>
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
